b38c3fda2603a1c88d0b4374c78686ac099ee27f
Kattis82 <kattiscalmvik@outlook.com>
2026-01-22 14:37:15 +0100

    UserController tillagd

--- src/main/java/se/iths/kattis/springboot/service/UserService.java
+++ src/main/java/se/iths/kattis/springboot/service/UserService.java
@@ -1,4 +1,12 @@
 package se.iths.kattis.springboot.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    public String userFromService() {
+        return "Hello from UserService!";
+    }
 }
+
+
