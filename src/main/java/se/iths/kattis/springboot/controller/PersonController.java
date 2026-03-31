45b112d1bb8d2efe3a0c9ac2b4faa1f5c46f2a9e
Kattis82 <kattiscalmvik@outlook.com>
2026-03-31 14:27:32 +0200

    lagt till textformatting-biblioteket

--- src/main/java/se/iths/kattis/springboot/controller/PersonController.java
+++ src/main/java/se/iths/kattis/springboot/controller/PersonController.java
@@ -6,19 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import se.iths.kattis.springboot.model.Person;
 import se.iths.kattis.springboot.service.PersonService;
+import se.iths.kattis.util.TextFormatter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/")
 public class PersonController {
 
     private final PersonService personService;
+    private final TextFormatter textFormatter;
 
-    public PersonController(PersonService personService) {
+    public PersonController(PersonService personService, TextFormatter textFormatter) {
         this.personService = personService;
+        this.textFormatter = textFormatter;
     }
-    
+
     // http://localhost:8080/persons
     @GetMapping("/persons")
     public String personList(Model model) {      // @ResponseBody tas bort före String för att använda HTML istället
@@ -26,7 +30,16 @@ public class PersonController {
                 new Person("20200727", "Oscar", 5, "oscarcalmvik@outlook.com"),
                 new Person("20231010", "Soya", 2, "soya@soya.se")
         );
-        model.addAttribute("persons", persons);
+        List<Person> formattedPersons = persons.stream()
+                .map(p -> new Person(
+                        p.getId(),
+                        textFormatter.textToUpper(p.getName()), // versaler
+                        p.getAge(),
+                        p.getEmail()
+                ))
+                .collect(Collectors.toList());
+
+        model.addAttribute("persons", formattedPersons);
         return "persons";
     }
 }
